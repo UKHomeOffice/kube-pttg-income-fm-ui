cec2d1c9d095d7a1d5063c1a5cf5bf279d16c45a
Stuart Craigen <s.craigen@mac.com>
2017-07-12 08:44:20 +0100

    removed the timeout flag

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
-kd --insecure-skip-tls-verify --retries=20 \
+kd --debug \
+   --insecure-skip-tls-verify --timeout 5m0s \
    --file ${ENVIRONMENT}/pttg-ip-fm-ui-deployment.yaml \
    --file ${ENVIRONMENT}/pttg-ip-fm-ui-ingress.yaml \
    --file ${ENVIRONMENT}/pttg-ip-fm-ui-svc.yaml
