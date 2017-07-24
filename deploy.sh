435fb8af191f6f8ae4efe46f7a93857d6985625c
Andy Marke <andy.marke@gmail.com>
2017-07-24 16:00:47 +0100

    rationalise kube config to make environment agnostic

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export ENVIRONMENT=${ENVIRONMENT}
-export APP=pttg-ip-fm-ui
 export KUBE_SERVER=${KUBE_SERVER_DEV}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
 kd --debug \
    --insecure-skip-tls-verify --timeout 5m0s \
-   --file ${ENVIRONMENT}/pttg-ip-fm-ui-deployment.yaml \
-   --file ${ENVIRONMENT}/pttg-ip-fm-ui-ingress.yaml \
-   --file ${ENVIRONMENT}/pttg-ip-fm-ui-svc.yaml
+   --file deployment.yaml \
+   --file ingress.yaml \
+   --file service.yaml
