cc60ea2040f3ae428666a89a5bf18069bdcf97eb
Andy Marke <andy.marke@gmail.com>
2017-07-25 13:24:33 +0100

    amend order so deployment is last

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export ENVIRONMENT=${ENVIRONMENT}
-export KUBE_SERVER=${KUBE_SERVER_DEV}
+export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 
 cd kd
 kd --debug \
    --insecure-skip-tls-verify --timeout 5m0s \
-   --file deployment.yaml \
    --file ingress.yaml \
-   --file service.yaml
+   --file service.yaml \
+   --file deployment.yaml
