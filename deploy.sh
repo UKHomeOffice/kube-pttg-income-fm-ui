c9e1aa9f48b77b7925d0a534704f2041c5360f75
Andy Marke <andy.marke@gmail.com>
2017-09-07 14:30:17 +0100

    added debug from prod deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
+env
 export KUBE_NAMESPACE=${KUBE_NAMESPACE}
 export ENVIRONMENT=${ENVIRONMENT}
 export KUBE_SERVER=${KUBE_SERVER}
 export KUBE_TOKEN=${KUBE_TOKEN}
 export WHITELIST=${WHITELIST:-0.0.0.0/0}
+echo "reset vars"
+env
+echo "KUBE_TOKEN token is $KUBE_TOKEN"
+echo "PROD_KUBE_TOKEN token is $PROD_KUBE_TOKEN"
 
 if [ $ENVIRONMENT == "prod" ]
 then
